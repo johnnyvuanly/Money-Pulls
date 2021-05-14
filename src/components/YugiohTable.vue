d515a871ffcdb2d07bc0db20190a5ad5ce186d9b
John L <johnny@example.com>
2021-05-14 11:16:32 -0500

    Updated dependencies, added CORS dependencies to hopefully resolve CORS issue in browser. Added vue.config.js file for proxy server to fix CORS issue. Changed how api call is written in Yugiohtable compnent

--- src/components/YugiohTable.vue
+++ src/components/YugiohTable.vue
@@ -6,19 +6,19 @@
         <table class="table table-hover">
             <thead>
                 <tr>
-                <th scope="col">Set</th>
-                <th scope="col">Price High</th>
-                <th scope="col">Price Low</th>
-                <th scope="col">Updated At</th>
+                    <th scope="col">Set</th>
+                    <th scope="col">Price High</th>
+                    <th scope="col">Price Low</th>
+                    <th scope="col">Updated At</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="card in cards" v-bind:key="card.data[0].print_tag" >
-                <th scope="row">{{card.data[0].print_tag}}</th>
-                <td>{{card.data[0]}}</td>
-                <td>{{card.data[0]}}</td>
-                <td>{{card.data[0]}}</td>
-            </tr>
+                <tr v-for="card in cards" v-bind:key="card.status">
+                    <th scope="row">{{card.data.status}}</th>
+                    <td>{{card.status}}</td>
+                    <td>{{card.status}}</td>
+                    <td>{{card.status}}</td>
+                </tr>
             </tbody>
         </table>
     </div>
@@ -26,6 +26,7 @@
 </template>
 
 <script>
+
     import axios from 'axios';
 
     export default {
@@ -34,18 +35,16 @@
         data() {
             return {
                 cards: null,
-            }; 
+            }
         }, 
         // Make API calls
-        created: function() {
+        mounted () {
             axios
-                .get('http://yugiohprices.com/api/get_card_prices/Dark%20Magician')
-                // Save our data to cards
-                .then(res => { 
-                this.cards = res.data;
-            })
+                .get('http://localhost:8080/api/get_card_prices/dark magician')
+                .then((response) => (this.cards = response.status))
         }
-    }
+    };
+
 </script>
 
 <style>
